e4661bbd161cfd0cdb167c9aeb5038434eaf9c0f
Jayant Saraswat <jayantsaraswat09@gmail.com>
2025-04-29 11:22:02 +0530

    Update App.jsx

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -11,13 +11,13 @@ import SignUp from './pages/Auth/SignUp';
 import Home from './pages/Dashboard/Home';
 import Income from './pages/Dashboard/Income';
 import Expense from './pages/Dashboard/Expense';
-import UserProvider from './context/userContext';
+import UserProvide from './context/userContext';
 import { Toaster } from "react-hot-toast";
 
 
 const App = () => {
   return (
-    <UserProvider>
+    <UserProvide>
       <div>
       <Router>
         <Routes>
@@ -41,7 +41,7 @@ const App = () => {
       />
 
 
-    </UserProvider>
+    </UserProvide>
   )
 }
 
