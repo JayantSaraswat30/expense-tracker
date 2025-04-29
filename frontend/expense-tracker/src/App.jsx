419c354d2eddc2e30fcb9bc51ed3e246418dd1a0
Jayant Saraswat <jayantsaraswat09@gmail.com>
2025-04-29 11:26:38 +0530

    Update App.jsx

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -11,7 +11,7 @@ import SignUp from './pages/Auth/SignUp';
 import Home from './pages/Dashboard/Home';
 import Income from './pages/Dashboard/Income';
 import Expense from './pages/Dashboard/Expense';
-import UserProvide from './context/userContext';
+import UserProvide from './context/UserContext';
 import { Toaster } from "react-hot-toast";
 
 
